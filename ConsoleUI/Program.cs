f3592f3c980a8dbedf74253a7bc9db6d69d9ae6e
Lenovo <Lenovo@DESKTOP-UO6M5AP>
2022-08-10 20:24:33 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarId + " / " + car.BrandId + " / " + car.ColorId + " / " + car.ModelYear + " / " + car.DailyPrice + " / " +
+                                  car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId = 1,BrandId = 1,ColorId = 1,ModelYear = 2021,DailyPrice = 150,Description = "Hasarlı"},
+                new Car{CarId = 2,BrandId = 2,ColorId = 2,ModelYear = 2022,DailyPrice = 200,Description = "Hasarsız"},
+                new Car{CarId = 3,BrandId = 3,ColorId = 2,ModelYear = 2011,DailyPrice = 450,Description = "Hasarlı"},
+                new Car{CarId = 4,BrandId = 2,ColorId = 3,ModelYear = 2021,DailyPrice = 300,Description = "Hasarsız"},
+                new Car{CarId = 5,BrandId = 1,ColorId = 1,ModelYear = 2019,DailyPrice = 100,Description = "Hasarlı"}
+            };
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetByBrandId(int brandId)
+        {
+            return _cars.Where(c=>c.BrandId==brandId).ToList();
+        }
+
+        public List<Car> GetByColorId(int colorId)
+        {
+            return _cars.Where(c=>c.ColorId==colorId).ToList();
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.CarId==car.CarId);
+            _cars.Remove(carToDelete);
+        }
+    }
+}
